6e3c00ee7bd50b78a58584cf342af4480139f1d8
Yurii Skrynnykov <truef1s7@gmail.com>
2021-04-29 04:05:38 +0300

    Add ligatures to st

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:size=10:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:size=10";
 static int borderpx = 1;
 
 /*
