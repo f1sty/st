14146f5003b3cc2094f6d2a8a36f361510f29335
Yurii Skrynnykov <truef1s7@gmail.com>
2023-11-27 13:27:15 +0200

    Move custom config to default

--- config.def.h
+++ config.def.h
@@ -134,7 +134,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
@@ -144,7 +144,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
